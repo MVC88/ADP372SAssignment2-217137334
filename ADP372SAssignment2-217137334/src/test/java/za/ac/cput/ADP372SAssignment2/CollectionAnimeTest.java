a3e495f9d016f62607aee3c89fa47b01615889ff
Malcolm Camelo <217137334@mycput.ac.za>
2021-05-15 22:56:39 +0200

    finished collection

---
+++ ADP372SAssignment2-217137334/src/test/java/za/ac/cput/ADP372SAssignment2/CollectionAnimeTest.java
@@ -0,0 +1,46 @@
+package za.ac.cput.ADP372SAssignment2;
+
+//Malcolm Camelo
+// student number :217137334
+
+import org.junit.jupiter.api.Test;
+import java.util.Collection;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollectionAnimeTest {
+
+    private CollectionAnime anime;
+
+    @Test
+    public void ColAdd() {  //Add
+        anime = new CollectionAnime();
+        Collection<String>  col1 = anime.getAnimeCollection();
+        col1.add("Owarimonogatari");
+
+        assert (col1).contains("Owarimonogatari");
+    }
+
+
+
+    @Test
+    public void ColRemove() {  //Remove
+        anime = new CollectionAnime();
+        Collection<String>  col1 = anime.getAnimeCollection();
+    col1.remove("Code Geass: Lelouch of the Rebellion");
+    assertFalse(col1.contains("Code Geass: Lelouch of the Rebellion"));
+    }
+
+    @Test
+    public void ColFind() {  //find
+        anime = new CollectionAnime();
+        Collection<String>  col1 = anime.getAnimeCollection();
+        boolean found = col1.contains("Chainsaw man");
+        assertTrue(found);
+
+    }
+
+
+
+}
